bcff1af947727602c5e75ca7472b93f6e7b727f3
SenyuDev <Yhusenvivas@gmail.com>
2024-10-09 00:33:31 +0200

    a

---
+++ main.js
@@ -0,0 +1,85 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+require('update-electron-app')
+const path = require('node:path')
+
+
+const { Client } = require("minecraft-launcher-core");
+
+const launcher = new Client();
+const { Auth } = require("msmc");
+const authManager = new Auth("select_account");
+
+const { forge } = require("tomate-loaders")
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+let prog = {
+    type: "assets",
+    task:0,
+    total:10
+}
+
+function launchMc(event) {
+    authManager.launch("electron").then(async (xboxManager) => {
+        const token = await xboxManager.getMinecraft();
+
+        const launchConfig = await forge.getMCLCLaunchConfig({
+            gameVersion: '1.16.5',
+            rootPath: './.minecraft',
+        });
+        let opts = {
+            ...launchConfig,
+            authorization: token.mclc(),
+            // aqui el link del modpack, de preferencia un link de dropbox, se descargara y descomprimira automaticamente
+            //clientPackage: "https://www.dropbox.com/s/1wzy2h1g4uz92sq/dungeon_server.zip?dl=1",
+            root: "./.minecraft",
+            memory: {
+                max: "4G",
+                min: "2G",
+            },
+            customArgs:[
+                "-XX:+UnlockExperimentalVMOptions",
+                "-XX:+UseG1GC",
+                "-XX:G1NewSizePercent=20",
+                "-XX:G1ReservePercent=20",
+                "-XX:MaxGCPauseMillis=50",
+                "-XX:G1HeapRegionSize=32M"
+              ]
+        };
+        console.log("Starting!");
+        launcher.launch(opts);
+
+        launcher.on("debug", (e) => console.log(e));
+        launcher.on("data", (e) => console.log(e));
+        launcher.on("progress", (e) => {
+            prog = e;
+            console.log(prog); // esto imprime el progreso en la consola principal
+            event.sender.send('progress-update', prog); // Envía el progreso al renderizador
+        });
+    });
+}
+
+app.whenReady().then(() => {
+    ipcMain.handle('ping', () => 'pong')
+    ipcMain.on('openButton', launchMc)
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
